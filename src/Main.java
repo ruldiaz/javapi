5677c448294236b1af2e9ffbbb4f8e044a45f023
ruldiaz <rulhdiaz@gmail.com>
2023-12-21 19:46:02 -0600

    second commit

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         // Press Alt+Intro with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!!!!!");
-
+        System.out.println("Hello again!");
 
     }
 }
